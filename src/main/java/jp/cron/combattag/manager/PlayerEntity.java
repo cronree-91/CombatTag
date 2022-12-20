8707db73c93c081be6f4d365fa3bef7db3eb7d28
cron <64413934+cronree-91@users.noreply.github.com>
2022-12-20 18:45:42 +0900

    Update PlayerEntity.java

--- src/main/java/jp/cron/combattag/manager/PlayerEntity.java
+++ src/main/java/jp/cron/combattag/manager/PlayerEntity.java
@@ -3,7 +3,7 @@ package jp.cron.combattag.manager;
 public class PlayerEntity {
 
 //    public boolean combatTag;
-    public Long lastCombat = System.currentTimeMillis();
+    public Long lastCombat = 0L;
     public boolean moveTag;
 
 }
